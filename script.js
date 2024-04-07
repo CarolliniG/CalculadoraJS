821eb221756b599c0dfd4d175bb18e0df7fd9138
Carollini_Gimenes <carollinig@gmail.com>
2024-04-06 21:20:16 -0300

    alteração js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function insert(num){
-  document.getElementsByClassName('result').innerHTML = num;
+function insert(num)
+{
+  //document.getElementByClassName('result').innerHTML = num;
+  function insert(num) {
+    const resultElement = document.getElementsByClassName("result")[0];
+    resultElement.innerHTML = num;
+  }
+  
 }
